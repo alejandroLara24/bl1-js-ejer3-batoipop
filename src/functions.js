1b8b902056dafd6b02abdfa4fc39b6bfd493c602
AlejandroLaraMoncho <alejandrolaram243@gmail.com>
2021-11-05 20:36:31 +0100

    ValidarForm

--- src/functions.js
+++ src/functions.js
@@ -31,10 +31,17 @@ function estrellas(numero) {
 
 function showPrice(price, discount_price) {
     if(discount_price) {
-        return `<span class="text-muted text-decoration-line-through">${toCurrency(price)}</span>${toCurrency(discount_price)}`
+        return `<span class="text-muted text-decoration-line-through">${toCurrency(price)}</span> ${toCurrency(discount_price)}`
     } else {
         return toCurrency(price)
     }
+}
+
+function putFuelCategory() {
+    document.getElementById('inputFuel').innerHTML =
+        typesOfFuel.forEach(element => {
+            `<option id='fuel-${element.id}'>${element.fuel}</option>`
+        });
 }
 
 function toCurrency(price) {
@@ -42,5 +49,129 @@ function toCurrency(price) {
 }
 
 function showProduct(producto) {
-    return console.log(producto)
+    document.getElementById('formData').classList.remove('hide')
+    document.getElementById('allProducts').classList.add('hide')
+    const name = document.getElementById('inputName')
+    const original_price = document.getElementById('inputOriginalPrice')
+    const discount_price = document.getElementById('inputDiscountPrice')
+    const km = document.getElementById('inputKm')
+    const stars = document.getElementById('starsRange')
+    name.setAttribute('value',producto.name)
+    original_price.setAttribute('value',producto.original_price)
+    discount_price.setAttribute('value',producto.discount_price)
+    km.setAttribute('value',producto.km)
+    if (producto.stars) {
+        stars.setAttribute('value',producto.stars)
+    } else {
+        stars.setAttribute('value',0)
+    }
+    name.setAttribute('readonly',true)
+    original_price.setAttribute('readonly',true)
+    discount_price.setAttribute('readonly',true)
+    km.setAttribute('readonly',true)
+    stars.setAttribute('disabled',true)
+}
+
+function removeReadOnly() {
+    const name = document.getElementById('inputName')
+    const original_price = document.getElementById('inputOriginalPrice')
+    const discount_price = document.getElementById('inputDiscountPrice')
+    const km = document.getElementById('inputKm')
+    const stars = document.getElementById('starsRange')
+    name.setAttribute('value','')
+    original_price.setAttribute('value','')
+    discount_price.setAttribute('value','')
+    km.setAttribute('value','')
+    stars.setAttribute('value','')
+    name.removeAttribute('readonly')
+    original_price.removeAttribute('readonly')
+    discount_price.removeAttribute('readonly')
+    km.removeAttribute('readonly')
+    stars.removeAttribute('disabled')
+}
+
+function renderFuel() {
+    const select = document.getElementById('inputFuel')
+    typesOfFuel.forEach(typeFuel => select.add(new Option(typeFuel.fuel,typeFuel.id)))
+}
+
+function setListeners() {
+    const newProdForm = document.getElementById('product')
+    const inputs = document.getElementsByTagName('input')
+    newProdForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+
+        const nameInput = document.getElementById('inputName')
+        checkInputValidation(nameInput)
+        if (nameInput.checkValidity()) {
+            if (isUsedName(nameInput.value)) {
+                nameInput.setCustomValidity('Ese nombre ya está en uso')
+                renderInputError(nameInput)
+            }
+        }
+        checkInputValidation(document.getElementById('inputOriginalPrice'))
+        checkInputValidation(document.getElementById('inputDiscountPrice'))
+        checkInputValidation(document.getElementById('inputKm'))
+
+        if (!newProdForm.checkValidity()) {
+            return
+        }
+
+        var http = new XMLHttpRequest();
+        let insertProduct = {
+            "name": String(document.getElementById('inputName').value), 
+            "km": Number(document.getElementById('inputKm').value), 
+            "original_price": Number(document.getElementById('inputOriginalPrice').value), 
+            "discount_price": Number(document.getElementById('inputDiscountPrice').value), 
+            "stars": Number(document.getElementById('starsRange').value), 
+            "sale": false, 
+            "fuel": Number(document.getElementById('inputFuel').value), 
+            "manual_gear": Boolean(document.querySelector('input[name=gear]:checked').value), 
+            "img": String(document.getElementById('inputPhoto').value)
+        }
+        http.open('POST', 'http://localhost:3000/products');
+        http.setRequestHeader('content-type', 'application/json');
+        http.send(JSON.stringify(insertProduct));
+        http.addEventListener('load', () => {
+            if (http.status === 200) {
+              console.log("Enviado con exito")
+            } else {
+              reject("Error " + this.status + " (" + this.statusText + ") en la petición");
+            }
+        })
+        http.addEventListener('error', () => reject('Error en la petición HTTP'));
+    
+    })
+    
+    document.getElementById('inputName').addEventListener('blur', (event) => {
+        event.preventDefault()
+        document.getElementById('errorName').innerHTML = inputs[0].validationMessage
+    }, true)
+    document.getElementById('inputOriginalPrice').addEventListener('blur', (event) => {
+        event.preventDefault()
+        document.getElementById('errorOriginal').innerHTML = inputs[1].validationMessage
+    }, true)
+    document.getElementById('inputDiscountPrice').addEventListener('blur', (event) => {
+        event.preventDefault()
+        document.getElementById('errorDiscount').innerHTML = inputs[2].validationMessage
+    }, true)
+    document.getElementById('inputKm').addEventListener('blur', (event) => {
+        event.preventDefault()
+        document.getElementById('errorKm').innerHTML = inputs[7].validationMessage
+    }, true)
+}
+
+function checkInputValidation(input) {
+    input.setCustomValidity('')
+    renderInputError(input)
+}
+
+function renderInputError(input) {
+    const errorSpan = input.nextElementSibling
+    errorSpan.innerHTML = input.validationMessage
+}
+
+function isUsedName(name) {
+    const prodFinded = products.find((prod) => prod.name === name)
+    return (prodFinded)
 }
--- src/index.js
+++ src/index.js
@@ -1,43 +1,98 @@
 'use strict'
-const productosDiv = document.getElementById('products')
-products.forEach(producto => {
-    let newProduct = document.createElement('div')
-    newProduct.className='col mb-5'
-    newProduct.innerHTML=`
-        <div class="card h-100">
-            <!-- Sale badge, sólo si está vendido-->
-            ${producto.sale?'<div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>':''}
-            <!-- Product image-->
-            <img class="card-img-top" src="${getImg(producto.img)}" alt="Imagen de _nombre_del_producto_" />
-            <!-- Product details-->
-            <div class="card-body p-4">
-                <div class="text-center">
-                    <!-- Product name-->
-                    <h5 class="fw-bolder">${producto.name}</h5>
-                    <!-- Product reviews, un div bi-star para cada estrella a pintar-->
-                    <div class="d-flex justify-content-center small text-warning mb-2">
-                        ${estrellas(producto.stars)}
+
+const SERVER = 'http://localhost:3000'
+let typesOfFuel = []
+let products = []
+
+window.addEventListener('load', () => {
+    getTypesOfFuel()
+
+    document.getElementById('home').addEventListener('click', () => {
+        document.getElementById('formData').classList.add('hide')
+        document.getElementById('allProducts').classList.remove('hide')
+    })
+    document.getElementById('new-prod').addEventListener('click', () => {
+        document.getElementById('formData').classList.remove('hide')
+        document.getElementById('allProducts').classList.add('hide')
+        removeReadOnly()
+    })
+    setListeners()
+})
+
+function getProducts() {
+    const peticion = new XMLHttpRequest();
+    peticion.open('GET', SERVER + '/products');
+    peticion.send();
+    peticion.addEventListener('load', () => {
+      if (peticion.status === 200) {
+        const datos = JSON.parse(peticion.responseText);
+        products = datos
+        renderProducts(datos)
+      } else {
+        reject("Error " + this.status + " (" + this.statusText + ") en la petición");
+      }
+    })
+    peticion.addEventListener('error', () => reject('Error en la petición HTTP'));
+}
+
+function getTypesOfFuel() {
+    const peticion = new XMLHttpRequest();
+    peticion.open('GET', SERVER + '/typesOfFuel');
+    peticion.send();
+    peticion.addEventListener('load', () => {
+      if (peticion.status === 200) {
+        const datos = JSON.parse(peticion.responseText);
+        typesOfFuel = datos
+        getProducts()
+        renderFuel()
+      } else {
+        reject("Error " + this.status + " (" + this.statusText + ") en la petición");
+      }
+    })
+    peticion.addEventListener('error', () => reject('Error en la petición HTTP'));
+}
+
+function renderProducts(datos) {
+    const productosDiv = document.getElementById('products')
+    datos.forEach(producto => {
+        let newProduct = document.createElement('div')
+        newProduct.className='col mb-5'
+        newProduct.innerHTML=`
+            <div class="card h-100">
+                <!-- Sale badge, sólo si está vendido-->
+                ${producto.sale?'<div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>':''}
+                <!-- Product image-->
+                <img class="card-img-top" src="${getImg(producto.img)}" alt="Imagen de _nombre_del_producto_" />
+                <!-- Product details-->
+                <div class="card-body p-4">
+                    <div class="text-center">
+                        <!-- Product name-->
+                        <h5 class="fw-bolder">${producto.name}</h5>
+                        <!-- Product reviews, un div bi-star para cada estrella a pintar-->
+                        <div class="d-flex justify-content-center small text-warning mb-2">
+                            ${estrellas(producto.stars)}
+                        </div>
+                        <!-- Product price-->
+                        ${showPrice(producto.original_price,producto.discount_price)}
+                        <!-- Product details -->
+                        <p>
+                            ${tipoDeCombustible(producto.fuel)}<br>
+                            ${producto.manual_gear?'Manual':'Automático'}
+                            <br>${producto.km.toLocaleString()} km
+                        </p>
                     </div>
-                    <!-- Product price-->
-                    ${showPrice(producto.original_price,producto.discount_price)}
-                    <!-- Product details -->
-                    <p>
-                        ${tipoDeCombustible(producto.fuel)}<br>
-                        ${producto.manual_gear?'Manual':'Automático'}
-                        <br>${producto.km.toLocaleString()} km
-                    </p>
+                </div>
+                <!-- Product actions-->
+                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                    <div class="text-center"><a class="mostrar btn btn-outline-dark mt-auto" href="#">Mostrar</a></div>
                 </div>
             </div>
-            <!-- Product actions-->
-            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                <div class="text-center"><a class="mostrar btn btn-outline-dark mt-auto" href="#">Mostrar</a></div>
-            </div>
-        </div>
-        `
-    productosDiv.appendChild(newProduct)
+            `
+        productosDiv.appendChild(newProduct)
 
-    newProduct.querySelector('.mostrar').addEventListener('click', () => {
-        event.preventDefault()
-        showProduct(producto)
+        newProduct.querySelector('.mostrar').addEventListener('click', (event) => {
+            event.preventDefault()
+            showProduct(producto)
+        })
     })
-})
+}
